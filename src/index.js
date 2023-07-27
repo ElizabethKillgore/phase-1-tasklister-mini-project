43ea542914b4067313ea7b46c18e2f5fcbf4fff1
Elizabeth <ek@elizabethkillgore.com>
2023-07-27 16:04:37 -0500

    tasklister

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  document.querySelector('form').addEventListener('submit', (e) => {
+    e.preventDefault()
+     fixMyTodos(e.target['new-task-description'].value)
+  })
 });
+
+
+function fixMyTodos(todos) {
+  let li = document.createElement('li')
+  let btn = document.createElement('button')
+  btn.textContent = 'x'
+  li.textContent = todos
+  li.append(btn)
+  document.getElementById('tasks').append(li)
+  btn.addEventListener('click', () => li.remove())
+}
+
+
+
+
+
+
+
+//need to prevent default behaviour of submit button with
+//event.preventDefault()
